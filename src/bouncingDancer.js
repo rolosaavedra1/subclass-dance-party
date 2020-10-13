84b924403a8d1cc4428c44930c0ff6f2f3602c74
meekoder <meekorusdi@gmail.com>
2020-10-13 00:57:57 -0700

    added boundingDancer

---
+++ src/bouncingDancer.js
@@ -0,0 +1,14 @@
+var BouncingDancer = function(top, left, timeBetweenSteps) {
+  Dancer.call(this, top, left, timeBetweenSteps, 'bouncingDancer');
+  this.timeBetweenSteps = timeBetweenSteps;
+  this.step();
+};
+
+BouncingDancer.prototype = Object.create(Dancer.prototype);
+
+BouncingDancer.prototype.constructor = BouncingDancer;
+
+BouncingDancer.prototype.step = function () {
+  Dancer.prototype.step.call(this);
+  this.$node.toggleClass('bounce');
+};
