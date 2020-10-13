104ff1ff39099df3d39956b9a8df8231b324216c
meekoder <meekorusdi@gmail.com>
2020-10-12 22:16:16 -0700

    added shyDancer class

--- src/shyDancer.js
+++ src/shyDancer.js
@@ -1,5 +1,16 @@
-const ShyDancer = function(top, left, timeBetweenSteps) {
+var = function(top, left, timeBetweenSteps){ //hahaha, what's going on her
   Dancer.call(this, top, left, timeBetweenSteps);
+  this.$node.addClass('shyDancer');
+  this.move();
+};
+ShyDancer.prototype = Object.create(Dancer.prototype);
+ShyDancer.prototype.constructor = ShyDancer;
+ShyDancer.prototype.move = function() {
+  // move on hover
+  Dancer.prototype.step.call(this);
+  this.$node.toggle();
+};
+  this.move();
 };
 
 ShyDancer.prototype = Object.create(Dancer.prototype);
@@ -8,4 +19,6 @@ ShyDancer.prototype.constructor = ShyDancer;
 
 ShyDancer.prototype.move = function() {
   // move on hover
-};
+  Dancer.prototype.step.call(this);
+  this.$node.toggle();
+}
