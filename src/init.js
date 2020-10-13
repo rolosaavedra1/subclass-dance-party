39b02ed18f7a29e8aa6179d8288d2ae4e05b32a9
meekoder <meekorusdi@gmail.com>
2020-10-13 12:58:33 -0700

    added positions array

--- src/init.js
+++ src/init.js
@@ -10,10 +10,23 @@ $(document).ready(function() {
       Math.random() * 1000
     );
     $('body').append(dancer.$node);
+    let positions = [];
+    const dancers = [...document.querySelectorAll('span')];
+    dancers.forEach(dancer => {
+      const rect = dancer.getBoundingClientRect();
+      const top = rect.top;
+      const left = rect.left;
+      const position = [top, left];
+      positions.push(position);
+    });
   });
   /*
   //select all dancer objects, update their .allDancerLocs property to the latest one
   document.querySelector('')
+  //go through dancer objects, collect their locations in an array [top, left]
+  // go through shyDancer objects, if the distance between them and any of the other locations in the array are greater than .minDistanceFromDancers, execute step()
+
+  let distance = Math.sqrt(Math.pow(arrayOfLocs[i][0] - this.top, 2) + Math.pow(arrayOfLocs[i][1] - this.left, 2));
    */
 });
 //iterate over objects, update their allDancers property to match the longest present
